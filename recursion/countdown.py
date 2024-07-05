dc02255d2730429dc089069172b3e657203b46f1
brunaportelaa <brunaporteladj@gmail.com>
2024-07-05 11:26:00 -0300

    Exercise on recursion - countdown.py

--- recursion/countdown.py
+++ recursion/countdown.py
@@ -1,1 +1,12 @@
 ## 03. Count Down: Write a recursive function that counts down from n to 1.
+def countdown(n):
+    if n > 0:
+        if n == 1:
+            print(1)
+            return 1
+        print (n)
+        countdown(n-1)
+    else:
+        return
+
+print(countdown(10))
