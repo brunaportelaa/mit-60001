cd6c17f2ee53725808414f4751ee8b94a0d19e21
brunaportelaa <brunaporteladj@gmail.com>
2024-07-09 18:39:45 -0300

    Created fibonacci.py

---
+++ recursion/fibonacci.py
@@ -0,0 +1,11 @@
+#8. Fibonacci: Write a recursive function to compute the nth Fibonacci number.
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(10))
---
+++ recursion/towers_of_hanoi.py
@@ -0,0 +1,3 @@
+#9. Write a recursive function to solve the Towers of Hanoi problem.
+
+#def towers_of_hanoi(n, source, target, spare):
