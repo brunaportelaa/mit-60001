9123155553046192e5444b430bd3c41d663ef6d9
brunaportelaa <brunaporteladj@gmail.com>
2024-07-10 20:48:31 -0300

    Update towers_of_hanoi.py

--- recursion/towers_of_hanoi.py
+++ recursion/towers_of_hanoi.py
@@ -1,3 +1,14 @@
 #9. Write a recursive function to solve the Towers of Hanoi problem.
 
-#def towers_of_hanoi(n, source, target, spare):
+
+def hanoi(n, source, target, spare):
+    if n == 1:
+        print(f"Move disk {n} from {source} to {target}")
+        return
+    else:
+        hanoi(n-1, source, spare, target)
+        print(f"Move disk {n} from {source} to {target}")
+        hanoi(n - 1, spare, target, source)
+
+
+hanoi(3, 'A', 'C', 'B')
